d6e8e01a8aa09d44dfd6ba00f8084967253af0fb
0xda157 <da157@voidq.com>
2025-12-02 16:53:56 -0800

    expose buildGleamApplication in packages flake output

--- flake.nix
+++ flake.nix
@@ -1,9 +1,17 @@
 {
   description = "nix-gleam - nix builder for gleam applications";
 
-  outputs = {
-    self,
-  }: {
-    overlays.default = import ./overlay.nix;
-  };
+  inputs.nixpkgs = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+
+      forEachPkgs = f: lib.genAttrs lib.systems.flakeExposed (system: f nixpkgs.legacyPackage.${system});
+    in
+    {
+      packages = forEachPkgs (pkgs: pkgs.callPackage ./builder { });
+      overlays.default = import ./overlay.nix;
+    };
 }

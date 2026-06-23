6a97c6514d6ec0a6b2abaed5de458b607fa67d97
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-06-23 13:30:52 -0400

    rework test for new builder and finalAttrs

--- tests/default.nix
+++ tests/default.nix
@@ -3,15 +3,41 @@
   newScope,
   buildGleamApplication,
 }:
+let
+  buildTest = lib.extendMkDerivation {
+    constructDrv = buildGleamApplication;
+
+    excludeDrvArgNames = [
+      "name"
+    ];
+
+    extendDrvArgs =
+      finalAttrs:
+      {
+        name,
+        version ? "none",
+        src ? ./${name},
+        ...
+      }:
+      {
+        pname = name + "-test";
+        inherit src version;
+      };
+  };
+in
 lib.makeScope newScope (self: {
-  basic = buildGleamApplication {
-    pname = "basic-test";
-    version = "none";
-    src = ./basic;
+  basic = buildTest {
+    name = "basic";
 
     doInstallCheck = true;
     installCheckPhase = ''
       "$out/bin/basic"
     '';
   };
+
+  finalAttrs = buildTest (finalAttrs: {
+    name = "finalAttrs-test";
+    version = "${finalAttrs.pname}-none";
+    src = ./basic;
+  });
 })
